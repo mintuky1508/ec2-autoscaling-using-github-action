a7784146d5c3ae649f796172a619afe555899972
mintuky1508 <mintuky1508@gmail.com>
2024-12-01 12:45:25 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,4 +1,19 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "security_groups" {}
-variable "subnet" {}
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+  type        = string
+}
+
+variable "security_groups" {
+  description = "The security group IDs for the EC2 instance"
+  type        = list(string)
+}
+
+variable "subnet" {
+  description = "The subnet ID for the EC2 instance"
+  type        = string
+}
